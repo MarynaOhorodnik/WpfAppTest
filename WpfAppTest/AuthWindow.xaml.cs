2ab6e8a27c5351f529daf730b9b90bc697abfc19
Maryna <m.ohorodnik.22@gmail.com>
2021-04-20 15:37:18 +0300

    Add project files.

---
+++ WpfAppTest/AuthWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppTest
+{
+    /// <summary>
+    /// Interaction logic for AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Некоректне значення";
+                textBoxLogin.Background = Brushes.MistyRose;
+            }
+
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Некоректне значення";
+                passBox.Background = Brushes.MistyRose;
+            }
+
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+
+
+                User authUser = null;
+                using(ApplicationContext db = new ApplicationContext())
+                {
+                    authUser = db.Users.Where(user => user.Login == login && user.Pass == pass).FirstOrDefault();
+                }
+
+                if (authUser != null)
+                {
+                    MessageBox.Show("Успішно!");
+                    UserPageWindow userPageWindow = new UserPageWindow();
+                    userPageWindow.Show();
+                    Hide();
+                }
+
+                else
+                    MessageBox.Show("Логін або пароль некоректні!");
+            }
+        }
+
+        private void Button_Reg_Window_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+    }
+}
---
+++ WpfAppTest/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ApplicationContext db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new ApplicationContext();
+
+            //List<User> users = db.Users.ToList();
+            //string str = "";
+            //foreach (User user in users)
+            //    str += "Login " + user.Login + " | ";
+
+
+            //exampleText.Text = str;
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass1 = passBox1.Password.Trim();
+            string pass2 = passBox2.Password.Trim();
+            string email = textBoxEmail.Text.Trim();
+
+            if(login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Некоректне значення";
+                textBoxLogin.Background = Brushes.MistyRose;
+            }
+
+            else if(pass1.Length < 5)
+            {
+                passBox1.ToolTip = "Некоректне значення";
+                passBox1.Background = Brushes.MistyRose;
+            }
+
+            else if (pass2 != pass1)
+            {
+                passBox2.ToolTip = "Некоректне значення";
+                passBox2.Background = Brushes.MistyRose;
+            }
+
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Некоректне значення";
+                textBoxEmail.Background = Brushes.MistyRose;
+            }
+
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox1.ToolTip = "";
+                passBox1.Background = Brushes.Transparent;
+                passBox2.ToolTip = "";
+                passBox2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Успішно!");
+
+                User user = new User(login, email, pass1);
+
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                AuthWindow authWindow = new AuthWindow();
+                authWindow.Show();
+                Hide();
+            }
+
+        }
+
+        private void Button_Auth_Window_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+            authWindow.Show();
+            Hide();
+        }
+    }
+}
